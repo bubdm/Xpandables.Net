a09e0d9398fe0fdf2ffb33f5b88fafaa4bcd7e9a
Francis-Black EWANE <fewane@efb-entreprises.com>
2020-07-11 19:43:06 +0200

    add managed extensibility

---
+++ Xpandables.Net5.DependencyInjection/ManagedExtensibility/EmptyCatalog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+
+namespace Xpandables.Standard.ManagedExtensibility
+{
+    /// <summary>
+    /// Defines a <see cref="ComposablePartCatalog"/> implementation that always returns an empty collection of parts.
+    /// </summary>
+    public sealed class EmptyCatalog : ComposablePartCatalog
+    {
+        /// <summary>
+        /// Gets the part definitions that are contained in the catalog.
+        /// </summary>
+        /// <returns>The <see cref="ComposablePartDefinition" /> contained in the <see cref="ComposablePartCatalog" />.</returns>
+        /// <exception cref="ObjectDisposedException">The <see cref="ComposablePartCatalog" /> object has been disposed of.</exception>
+        public override IQueryable<ComposablePartDefinition> Parts => Enumerable.Empty<ComposablePartDefinition>().AsQueryable();
+    }
+}
---
+++ Xpandables.Net5.DependencyInjection/ManagedExtensibility/ServiceCollectionExtensions.cs
@@ -0,0 +1,113 @@
+﻿
+/************************************************************************************************************
+ * Copyright (C) 2020 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+using System;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Reflection;
+
+using Xpandables.Net5.DependencyInjection.ManagedExtensibility;
+using Xpandables.Standard.ManagedExtensibility;
+
+#pragma warning disable ET002 // Namespace does not match file path or default namespace
+namespace Microsoft.Extensions.DependencyInjection
+#pragma warning restore ET002 // Namespace does not match file path or default namespace
+{
+    /// <summary>
+    /// Provides method to register exports.
+    /// </summary>
+    public static partial class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds and configures registration of services using the <see cref="IAddServiceExport"/> implementations found in the current application path.
+        /// This method is used with MEF : Managed Extensibility Framework.
+        /// </summary>
+        /// <param name="services">The collection of services.</param>
+        /// <exception cref="ArgumentNullException">The <paramref name="services"/> is null.</exception>
+        /// <exception cref="InvalidOperationException">The operation failed. See inner exception.</exception>
+        public static IServiceCollection AddXServiceExport(this IServiceCollection services)
+        {
+            if (services is null) throw new ArgumentNullException(nameof(services));
+            services.AddXServiceExport(_ => { });
+            return services;
+        }
+
+        /// <summary>
+        /// Adds and configures registration of services using the<see cref="IAddServiceExport"/> implementations found in the path.
+        /// This method is used with MEF : Managed Extensibility Framework.
+        /// </summary>
+        /// <param name="services">The collection of services.</param>
+        /// <param name="configureOptions">A delegate to configure the <see cref="ExportServiceOptions"/>.</param>
+        /// <exception cref="ArgumentNullException">The <paramref name="services"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="configureOptions"/> is null.</exception>
+        /// <exception cref="InvalidOperationException">The operation failed. See inner exception.</exception>
+        public static IServiceCollection AddXServiceExport(
+            this IServiceCollection services, Action<ExportServiceOptions> configureOptions)
+        {
+            if (services is null) throw new ArgumentNullException(nameof(services));
+            if (configureOptions == null) throw new ArgumentNullException(nameof(configureOptions));
+
+            var definedOptions = new ExportServiceOptions();
+            configureOptions.Invoke(definedOptions);
+            services.AddServiceExport(definedOptions);
+
+            return services;
+        }
+
+        private static void AddServiceExport(this IServiceCollection services, ExportServiceOptions options)
+        {
+            try
+            {
+                using var directoryCatalog = options.SearchSubDirectories
+                    ? new RecursiveDirectoryCatalog(options.Path, options.SearchPattern)
+                    : (ComposablePartCatalog)new DirectoryCatalog(options.Path, options.SearchPattern);
+
+                var importDefinition = BuildAddImportDefinition();
+
+                using var aggregateCatalog = new AggregateCatalog();
+                aggregateCatalog.Catalogs.Add(directoryCatalog);
+
+                using var compositionContainer = new CompositionContainer(aggregateCatalog);
+                var exportServices = compositionContainer
+                    .GetExports(importDefinition)
+                    .Select(def => def.Value)
+                    .OfType<IAddServiceExport>();
+
+                foreach (var export in exportServices)
+                    export.AddServices(services);
+            }
+            catch (Exception exception) when (exception is NotSupportedException
+                                            || exception is System.IO.DirectoryNotFoundException
+                                            || exception is UnauthorizedAccessException
+                                            || exception is ArgumentException
+                                            || exception is System.IO.PathTooLongException
+                                            || exception is ReflectionTypeLoadException)
+            {
+                throw new InvalidOperationException("Adding exports failed. See inner exception.", exception);
+            }
+        }
+
+        private static ImportDefinition BuildAddImportDefinition()
+            => new ImportDefinition(
+                    _ => true,
+                    typeof(IAddServiceExport).FullName,
+                    ImportCardinality.ZeroOrMore,
+                    false,
+                    false);
+    }
+}
