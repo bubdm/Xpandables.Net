17caa3998d88d02bdac492b1d508afb37f889c60
Francis-Black EWANE <fewane@efb-entreprises.com>
2021-04-05 14:23:35 +0200

    add registration header accessor

---
+++ Xpandables.Net.AspNetCore/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿
+/************************************************************************************************************
+ * Copyright (C) 2020 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+
+using Xpandables.Net.Http;
+
+namespace Xpandables.Net.DependencyInjection
+{
+    /// <summary>
+    /// Provides method to register services.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the specified HTTP request header values accessor that implements the <see cref="IHttpHeaderAccessor"/>.
+        /// </summary>
+        /// <param name="services">The collection of services.</param>
+        /// <exception cref="ArgumentNullException">The <paramref name="services"/> is null.</exception>
+        public static IServiceCollection AddXHttpHeaderAccessor(this IServiceCollection services)
+        {
+            _ = services ?? throw new ArgumentNullException(nameof(services));
+
+            services.AddScoped<IHttpHeaderAccessor, HttpHeaderAccessor>();
+            return services;
+        }
+    }
+}
