e840b078346997cc897379d4eebf9111e3c3ecb5
Francis-Black EWANE <fewane@efb-entreprises.com>
2021-06-18 09:08:15 +0200

    move criteria out

--- Xpandables.Net/Aggregates/IEventStore.cs
+++ Xpandables.Net/Aggregates/IEventStore.cs
@@ -17,60 +17,13 @@
 ************************************************************************************************************/
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
 using Xpandables.Net.DomainEvents;
-using Xpandables.Net.Expressions;
 
 namespace Xpandables.Net.Aggregates
 {
-    /// <summary>
-    /// Provides with criteria to search for store entities.
-    /// This class derive from <see cref="QueryExpression{TSource}"/>.
-    /// </summary>
-    /// <typeparam name="TStoreEntity">The type of the store entity to be returned.</typeparam>
-    public class StoreEntityCriteria<TStoreEntity> : QueryExpression<TStoreEntity>
-        where TStoreEntity : StoreEntity
-    {
-        /// <summary>
-        /// Gets the type name as <see cref="Regex"/> format. If null, all types will be returned.
-        /// </summary>
-        public string? TypeName { get; }
-
-        /// <summary>
-        /// Gets the date to start search. If null, starts from the beginning.
-        /// </summary>
-        public DateTime? Start { get; }
-
-        /// <summary>
-        /// Gets the date to end search.
-        /// </summary>
-        public DateTime? End { get; }
-
-        /// <summary>
-        /// Gets the number of entities to be returned.
-        /// The default value is 50.
-        /// </summary>
-        public int Count { get; } = 50;
-
-        ///<inheritdoc/>
-        public override Expression<Func<TStoreEntity, bool>> GetExpression()
-        {
-            var expression = QueryExpressionFactory.Create<TStoreEntity>();
-            if (TypeName is not null)
-                expression = expression.And(se => Regex.IsMatch(se.TypeName, TypeName));
-            if (Start is not null)
-                expression = expression.And(se => se.CreatedOn >= Start.Value);
-            if (End is not null)
-                expression = expression.And(se => se.CreatedOn <= End.Value);
-
-            return expression;
-        }
-    }
-
     /// <summary>
     /// Provides with methods to retrieve and persist events.
     /// </summary>
