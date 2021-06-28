8b5bf57b776bd5237f1b41309742f83086979f6c
Francis-Black EWANE <fewane@efb-entreprises.com>
2021-06-28 20:55:53 +0200

    refacto newtosnsoft

--- Xpandables.Net.DependencyInjection/Http/RequestBuilders/HttpRestClientNewtonSoftRequestBuilder.cs
+++ Xpandables.Net.DependencyInjection/Http/RequestBuilders/HttpRestClientNewtonSoftRequestBuilder.cs
@@ -15,13 +15,7 @@
  * limitations under the License.
  *
 ************************************************************************************************************/
-using Newtonsoft.Json;
-
-using System.IO;
 using System.Net.Http;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Xpandables.Net.Http.RequestBuilders
 {
@@ -30,40 +24,40 @@ namespace Xpandables.Net.Http.RequestBuilders
     /// </summary>
     public class HttpRestClientNewtonsoftRequestBuilder : HttpRestClientRequestBuilder
     {
-        ///<inheritdoc/>
-        public override async Task<HttpContent?> ReadStreamContentAsync<TSource>(
-            TSource source, JsonSerializerOptions? serializerOptions = default)
-            where TSource : class
-        {
-            ValidateInterfaceImplementation<IStreamRequest>(source);
-            object streamContent = source is IStreamRequest streamRequest ? streamRequest.GetStreamContent() : source;
+        /////<inheritdoc/>
+        //public override async Task<HttpContent?> ReadStreamContentAsync<TSource>(
+        //    TSource source, JsonSerializerOptions? serializerOptions = default)
+        //    where TSource : class
+        //{
+        //    ValidateInterfaceImplementation<IStreamRequest>(source);
+        //    object streamContent = source is IStreamRequest streamRequest ? streamRequest.GetStreamContent() : source;
 
-            var memoryStream = new MemoryStream();
-            await using var streamWriter = new StreamWriter(memoryStream, new UTF8Encoding(false), 1028, true);
-            using var jsonTextWriter = new JsonTextWriter(streamWriter) { Formatting = Formatting.None };
-            Newtonsoft.Json.JsonSerializer.CreateDefault().Serialize(jsonTextWriter, streamContent);
-            await jsonTextWriter.FlushAsync().ConfigureAwait(false);
+        //    var memoryStream = new MemoryStream();
+        //    await using var streamWriter = new StreamWriter(memoryStream, new UTF8Encoding(false), 1028, true);
+        //    using var jsonTextWriter = new JsonTextWriter(streamWriter) { Formatting = Formatting.None };
+        //    Newtonsoft.Json.JsonSerializer.CreateDefault().Serialize(jsonTextWriter, streamContent);
+        //    await jsonTextWriter.FlushAsync().ConfigureAwait(false);
 
-            memoryStream.Seek(0, SeekOrigin.Begin);
-            return new StreamContent(memoryStream);
-        }
+        //    memoryStream.Seek(0, SeekOrigin.Begin);
+        //    return new StreamContent(memoryStream);
+        //}
 
-        ///<inheritdoc/>
-        public override HttpContent ReadStringContent<TSource>(
-            TSource source,
-            HttpRestClientAttribute attribute,
-            JsonSerializerOptions? serializerOptions = default)
-            where TSource : class
-        {
-            ValidateInterfaceImplementation<IStringRequest>(source, true);
-            if (source is IStringRequest stringRequest)
-                return new StringContent(JsonConvert.SerializeObject(stringRequest.GetStringContent()), Encoding.UTF8, attribute.ContentType);
+        /////<inheritdoc/>
+        //public override HttpContent ReadStringContent<TSource>(
+        //    TSource source,
+        //    HttpRestClientAttribute attribute,
+        //    JsonSerializerOptions? serializerOptions = default)
+        //    where TSource : class
+        //{
+        //    ValidateInterfaceImplementation<IStringRequest>(source, true);
+        //    if (source is IStringRequest stringRequest)
+        //        return new StringContent(JsonConvert.SerializeObject(stringRequest.GetStringContent()), Encoding.UTF8, attribute.ContentType);
 
-            ValidateInterfaceImplementation<IPatchRequest>(source, true);
-            if (source is IPatchRequest patchRequest)
-                return new StringContent(JsonConvert.SerializeObject(patchRequest.GetPatchDocument()), Encoding.UTF8, attribute.ContentType);
+        //    ValidateInterfaceImplementation<IPatchRequest>(source, true);
+        //    if (source is IPatchRequest patchRequest)
+        //        return new StringContent(JsonConvert.SerializeObject(patchRequest.GetPatchDocument()), Encoding.UTF8, attribute.ContentType);
 
-            return new StringContent(JsonConvert.SerializeObject(source), Encoding.UTF8, attribute.ContentType);
-        }
+        //    return new StringContent(JsonConvert.SerializeObject(source), Encoding.UTF8, attribute.ContentType);
+        //}
     }
 }
--- Xpandables.Net.DependencyInjection/Http/ResponseBuilders/HttpRestClientNewtonSoftResponseBuilder.cs
+++ Xpandables.Net.DependencyInjection/Http/ResponseBuilders/HttpRestClientNewtonSoftResponseBuilder.cs
@@ -1,5 +1,4 @@
-﻿
-/************************************************************************************************************
+﻿/************************************************************************************************************
  * Copyright (C) 2020 Francis-Black EWANE
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,16 +14,6 @@
  * limitations under the License.
  *
 ************************************************************************************************************/
-using Newtonsoft.Json;
-
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-using System.Runtime.CompilerServices;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Xpandables.Net.Http.ResponseBuilders
 {
@@ -33,99 +22,99 @@ namespace Xpandables.Net.Http.ResponseBuilders
     /// </summary>
     public class HttpRestClientNewtonsoftResponseBuilder : HttpRestClientResponseBuilder
     {
-        ///<inheritdoc/>
-        public override async Task<HttpRestClientResponse<IAsyncEnumerable<TResult>>>
-             WriteAsyncEnumerableResponseAsync<TResult>(
-             HttpResponseMessage httpResponse,
-             JsonSerializerOptions? serializerOptions = default,
-             CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                var stream = await httpResponse.Content
-                    .ReadAsStreamAsync(cancellationToken)
-                    .ConfigureAwait(false);
+        /////<inheritdoc/>
+        //public override async Task<HttpRestClientResponse<IAsyncEnumerable<TResult>>>
+        //     WriteAsyncEnumerableResponseAsync<TResult>(
+        //     HttpResponseMessage httpResponse,
+        //     JsonSerializerOptions? serializerOptions = default,
+        //     CancellationToken cancellationToken = default)
+        //{
+        //    try
+        //    {
+        //        var stream = await httpResponse.Content
+        //            .ReadAsStreamAsync(cancellationToken)
+        //            .ConfigureAwait(false);
 
-                if (stream is null)
-                {
-                    return HttpRestClientResponse<IAsyncEnumerable<TResult>>
-                        .Success(AsyncEnumerableExtensions.Empty<TResult>(), httpResponse.StatusCode)
-                        .AddHeaders(httpResponse.ReadHttpResponseHeaders())
-                        .AddVersion(httpResponse.Version)
-                        .AddReasonPhrase(httpResponse.ReasonPhrase);
-                }
+        //        if (stream is null)
+        //        {
+        //            return HttpRestClientResponse<IAsyncEnumerable<TResult>>
+        //                .Success(AsyncEnumerableExtensions.Empty<TResult>(), httpResponse.StatusCode)
+        //                .AddHeaders(httpResponse.ReadHttpResponseHeaders())
+        //                .AddVersion(httpResponse.Version)
+        //                .AddReasonPhrase(httpResponse.ReasonPhrase);
+        //        }
 
-                var results = AsyncEnumerableBuilderAsync(stream, serializerOptions, cancellationToken);
+        //        var results = AsyncEnumerableBuilderAsync(stream, serializerOptions, cancellationToken);
 
-                return HttpRestClientResponse<IAsyncEnumerable<TResult>>
-                    .Success(results, httpResponse.StatusCode)
-                    .AddHeaders(httpResponse.ReadHttpResponseHeaders())
-                    .AddVersion(httpResponse.Version)
-                    .AddReasonPhrase(httpResponse.ReasonPhrase);
-            }
-            catch (Exception exception)
-            {
-                return HttpRestClientResponse<IAsyncEnumerable<TResult>>
-                    .Failure(exception)
-                    .AddHeaders(httpResponse.ReadHttpResponseHeaders())
-                    .AddVersion(httpResponse.Version)
-                    .AddReasonPhrase(httpResponse.ReasonPhrase);
-            }
+        //        return HttpRestClientResponse<IAsyncEnumerable<TResult>>
+        //            .Success(results, httpResponse.StatusCode)
+        //            .AddHeaders(httpResponse.ReadHttpResponseHeaders())
+        //            .AddVersion(httpResponse.Version)
+        //            .AddReasonPhrase(httpResponse.ReasonPhrase);
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        return HttpRestClientResponse<IAsyncEnumerable<TResult>>
+        //            .Failure(exception)
+        //            .AddHeaders(httpResponse.ReadHttpResponseHeaders())
+        //            .AddVersion(httpResponse.Version)
+        //            .AddReasonPhrase(httpResponse.ReasonPhrase);
+        //    }
 
-            async static IAsyncEnumerable<TResult> AsyncEnumerableBuilderAsync(
-                Stream stream,
-                JsonSerializerOptions? serializerOptions = default,
-                [EnumeratorCancellation] CancellationToken cancellationToken = default)
-            {
-                var jsonSerializer = Newtonsoft.Json.JsonSerializer.CreateDefault();
-                using var streamReader = new StreamReader(stream);
-                using var jsonTextReader = new JsonTextReader(streamReader);
-                while (await jsonTextReader.ReadAsync(cancellationToken).ConfigureAwait(false))
-                {
-                    if (jsonTextReader.TokenType != JsonToken.StartObject) continue;
-                    if (jsonSerializer.Deserialize<TResult>(jsonTextReader) is { } result)
-                        yield return result;
-                }
-            }
-        }
+        //    async static IAsyncEnumerable<TResult> AsyncEnumerableBuilderAsync(
+        //        Stream stream,
+        //        JsonSerializerOptions? serializerOptions = default,
+        //        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        //    {
+        //        var jsonSerializer = Newtonsoft.Json.JsonSerializer.CreateDefault();
+        //        using var streamReader = new StreamReader(stream);
+        //        using var jsonTextReader = new JsonTextReader(streamReader);
+        //        while (await jsonTextReader.ReadAsync(cancellationToken).ConfigureAwait(false))
+        //        {
+        //            if (jsonTextReader.TokenType != JsonToken.StartObject) continue;
+        //            if (jsonSerializer.Deserialize<TResult>(jsonTextReader) is { } result)
+        //                yield return result;
+        //        }
+        //    }
+        //}
 
-        ///<inheritdoc/>
-        public override async Task<HttpRestClientResponse<TResult>> WriteSuccessResultResponseAsync<TResult>(
-            HttpResponseMessage httpResponse,
-            JsonSerializerOptions? serializerOptions = default,
-            CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                var stream = await httpResponse.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
-                if (stream is null)
-                {
-                    return HttpRestClientResponse<TResult>
-                        .Success(httpResponse.StatusCode)
-                        .AddHeaders(httpResponse.ReadHttpResponseHeaders())
-                        .AddVersion(httpResponse.Version)
-                        .AddReasonPhrase(httpResponse.ReasonPhrase);
-                }
+        /////<inheritdoc/>
+        //public override async Task<HttpRestClientResponse<TResult>> WriteSuccessResultResponseAsync<TResult>(
+        //    HttpResponseMessage httpResponse,
+        //    JsonSerializerOptions? serializerOptions = default,
+        //    CancellationToken cancellationToken = default)
+        //{
+        //    try
+        //    {
+        //        var stream = await httpResponse.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
+        //        if (stream is null)
+        //        {
+        //            return HttpRestClientResponse<TResult>
+        //                .Success(httpResponse.StatusCode)
+        //                .AddHeaders(httpResponse.ReadHttpResponseHeaders())
+        //                .AddVersion(httpResponse.Version)
+        //                .AddReasonPhrase(httpResponse.ReasonPhrase);
+        //        }
 
-                using var streamReader = new StreamReader(stream);
-                using var jsonTextReader = new JsonTextReader(streamReader);
+        //        using var streamReader = new StreamReader(stream);
+        //        using var jsonTextReader = new JsonTextReader(streamReader);
 
-                var result = Newtonsoft.Json.JsonSerializer.CreateDefault().Deserialize<TResult>(jsonTextReader);
+        //        var result = Newtonsoft.Json.JsonSerializer.CreateDefault().Deserialize<TResult>(jsonTextReader);
 
-                return HttpRestClientResponse<TResult>
-                    .Success(result, httpResponse.StatusCode)
-                    .AddHeaders(httpResponse.ReadHttpResponseHeaders())
-                    .AddVersion(httpResponse.Version)
-                    .AddReasonPhrase(httpResponse.ReasonPhrase);
-            }
-            catch (Exception exception)
-            {
-                return HttpRestClientResponse<TResult>
-                    .Failure(exception)
-                    .AddHeaders(httpResponse.ReadHttpResponseHeaders())
-                    .AddVersion(httpResponse.Version)
-                    .AddReasonPhrase(httpResponse.ReasonPhrase);
-            }
-        }
+        //        return HttpRestClientResponse<TResult>
+        //            .Success(result, httpResponse.StatusCode)
+        //            .AddHeaders(httpResponse.ReadHttpResponseHeaders())
+        //            .AddVersion(httpResponse.Version)
+        //            .AddReasonPhrase(httpResponse.ReasonPhrase);
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        return HttpRestClientResponse<TResult>
+        //            .Failure(exception)
+        //            .AddHeaders(httpResponse.ReadHttpResponseHeaders())
+        //            .AddVersion(httpResponse.Version)
+        //            .AddReasonPhrase(httpResponse.ReasonPhrase);
+        //    }
+        //}
     }
 }
