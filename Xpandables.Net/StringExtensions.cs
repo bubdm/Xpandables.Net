470180f2fc5544d1304c0ccd0f81468be8319ef7
Francis-Black EWANE <fewane@efb-entreprises.com>
2021-07-02 13:28:01 +0200

    remove unused ref

--- Xpandables.Net/StringExtensions.cs
+++ Xpandables.Net/StringExtensions.cs
@@ -22,10 +22,9 @@ using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
 using System.Text.Json;
-using System.Threading.Tasks;
-using System.Threading;
 using System.Text.Json.Serialization;
-using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Xpandables.Net
 {
